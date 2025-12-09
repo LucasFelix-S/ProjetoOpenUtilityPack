6094fcd7f2bd090c31455bbf047e7674bcfbcef1
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2025-12-08 21:13:31 -0300

    Criando uma classe abstrata para operações com o Banco de Dados

--- src/br/com/lucas/openutilitypack/app/App.java
+++ src/br/com/lucas/openutilitypack/app/App.java
@@ -1,19 +1,8 @@
 package br.com.lucas.openutilitypack.app;
 import br.com.lucas.openutilitypack.database.config.DataBaseManager;
-import br.com.lucas.openutilitypack.file.ReadingTextFile;
-import br.com.lucas.openutilitypack.file.WritingTextFile;
 
 public class App {
     public static void main(String[] args) {
-        String arquivo = "teste.txt";
-        ReadingTextFile.showTextFile(arquivo);
-
-        WritingTextFile writingTextFileNull = new WritingTextFile(null);
-        writingTextFileNull.writeTextFile("arquivoPadraoNull.txt");
-
-        WritingTextFile writingTextFile = new WritingTextFile("Escrevendo algo...");
-        writingTextFile.writeTextFile("arquivoPadrao.txt");
-
         DataBaseManager.connectionDataBase();
     }
 }
