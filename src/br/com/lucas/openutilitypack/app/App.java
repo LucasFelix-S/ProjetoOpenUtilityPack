867ad8d43b219c0975ac02c028d53080eb5f13f7
LucasFelix-S <started.lucasfelixsantos@gmail.com>
2025-12-25 13:43:26 -0300

    remove: removendo a classe 'app' que continha o método main, pois como o objetivo do projeto é ser uma pequena biblioteca e não uma aplicação, sendo assim, o método main se torna obsoleto

--- src/br/com/lucas/openutilitypack/app/App.java
+++
@@ -1,8 +0,0 @@
-package br.com.lucas.openutilitypack.app;
-import br.com.lucas.openutilitypack.database.DataBaseManager;
-
-public class App {
-    public static void main(String[] args) {
-        DataBaseManager.connectionDataBase();
-    }
-}
